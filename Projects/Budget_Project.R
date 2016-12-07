43ac3c8bf0de6e96461cea964dcb111fe7ba41ec
MomSweetMom <trunght91@gmail.com>
2016-12-07 15:28:53 +0100

    Project update 3

--- Projects/Budget_Project.R
+++ Projects/Budget_Project.R
@@ -1,25 +1,36 @@
+#### Packages ####
+library(reshape2)
+library(magrittr)
+library(ggplot2)
+install.packages("devtools")
+install.packages("Rcpp")
+library(devtools)
+library(Rcpp)
+install_github('ramnathv/rCharts')
+library(rCharts)
+install.packages("dplyr")
+install.packages("tidyr")
+library(dplyr)
+library(tidyr)
 
-
-# Load the data set
+#### Load the data set ####
 Budget <- read.csv("~/Downloads/Budget.txt", header=FALSE, stringsAsFactors=FALSE)
 View(Budget)
 par(family ="sans-serif")
 dim(Budget)
 colnames(Budget) = c("AN", "PVP", "AGR", "CMI", "TRA", "LOG", "EDU", "ACS", "ANC", "DEF", "DET", "DIV")
-#rownames(Budget) = Budget$AN
+# rownames(Budget) = Budget$AN
 
 
-# examine the structure of the data set.
+# Examine the structure of the data set.
 str(Budget)
 dim(Budget)
 rownames(Budget) = Budget$AN
 row.names(Budget)
 Budget[,-1]
 
-
 # Remove missing observations if any
 Budget = na.omit(Budget)
-
 
 # Check the variables names
 names(Budget)
@@ -28,28 +39,35 @@ boxplot(Budget[,-1])
 # Attach the observations Frame
 attach(Budget)
 
-#========================
-# Univariate Analysis
-#========================
+
+#===========================#
+#### Univariate Analysis ####
+#===========================#
 # Here we consider each variable separately.
 
 summary(Budget)
-
-# ------ combine multiple plots into one overall graph -----------
+summary(lm(AN~.,Budget))
+# ------ Combine multiple plots into one overall graph -----------
+#     Data overview    
 #     Show the distribution as a boxplot 
 #     Plot the histogram with, with kernel density estimators. 
 #     Make a quantile-quantile plot for both the normal and lognormal distributions.
 
-plot_plus<-function(a_var){
-  # 4 figures arranged in 2 rows and 2 columns
-  par(mfrow=c(1,2))
-  boxplot(a_var, main = ("Boxplot"), horizontal = TRUE, notch = FALSE)
-  #lines(density(a_var), col="red")
-  #lines(density(a_var, adjust = .5),col="blue") # ‘half the default’ bandwidth
-  #lines(density(a_var, adjust = 2),col="green")
-}
-boxplot("Budget")
+#### INTERACTIVE DATA OVERVIEW ####
+View(Budget)
+long_data <- Budget %>% gather(Type, Allocation, -AN)
+View(long_data)
+n <- nPlot(Allocation ~ AN, data = long_data, 
+           group = "Type", type = "lineChart")
+n$chart(yDomain = sort(range(long_data$Allocation), 
+                       decreasing = T) )
+n$chart(interpolate = "step-after")
+n$xAxis(axisLabel = "Year")
+n$yAxis(axisLabel = "Allocation")
+n$chart(margin = list(left = 75))
+n
 
+# Histogram graphic of each variable
 par(mfrow=c(3,4))
 for (i in 2:length(names(Budget))){
   hist(scale(Budget)[,i],main=names(Budget)[i])
@@ -58,6 +76,7 @@ for (i in 2:length(names(Budget))){
   lines(density(Budget[,i], adjust = 2),col="green")
 }
 
+# Histogram graphic of
 par(mfrow=c(3,4),family ='sans')
 for (i in 2:length(names(Budget))){
   hist((Budget)[,i],main=names(Budget)[i],prob=TRUE,xlab='% of the total Budget')
@@ -66,25 +85,28 @@ for (i in 2:length(names(Budget))){
   #lines(density(Budget[,i], adjust = 2),col="green")
 }
 
-
-par(mfrow=c(3,4))
-
+# Check normalization
 c_d<-names(Budget)
-
 for (i in 2:length(names(Budget))){
   qqnorm((Budget)[,i], main=paste(c_d[i],'\n', 'vs','\n', 'Normal dist.'), ylab = c_d[i])
   qqline((Budget)[,i], col= "green")
 }
 
-
-
-# HEATMAP
+#### HEATMAP ####
 cormat<-round(cor(Budget),2)
 #cormat<-get_upper_tri(cormat)
 cormat<-melt(cormat)
 
-ggplot(data=data.frame(cormat),aes(fill=value,x=X1, y=X2))+geom_tile(color="white")+
-  scale_fill_gradient2(low = "blue", high = "blue", mid = "orange", midpoint = 0, limit = c(-1,1), space = "Lab",name="Pearson\nCorrelation")
+ggplot(data=data.frame(cormat),
+       aes(fill=value,x=X1, y=X2)) +
+       geom_tile(color="white") +
+       scale_fill_gradient2(low = "blue", 
+                            high = "blue", 
+                            mid = "orange", 
+                            midpoint = 0, 
+                            limit = c(-1,1), 
+                            space = "Lab",
+                            name="Pearson\nCorrelation")
 
 # minimal theme 
 
